30d1ff14daef0bf6c825c3fc1ce705527f780d33
Jason Liebault <jason.liebault@simsoft-industry.fr>
2020-12-11 15:16:25 +0100

    Add test scenario Add stock order firstfruit

---
+++ SuperCaisse.Services/StockOrderService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperCaisse.Services
+{
+    public class StockOrderService
+    {
+        public string[] AddStockOrder(params string[] articles)
+        {
+            return articles;
+        }
+    }
+}
