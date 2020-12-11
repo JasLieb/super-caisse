19e6d46079a152bcec612d0c751a881b71bf27e6
Jason Liebault <jason.liebault@simsoft-industry.fr>
2020-12-11 12:40:07 +0100

    Add new project for model

--- SuperCaisse.Services.Tests/Class1/Class1Test.Hello.cs
+++ SuperCaisse.Services.Tests/Class1/Class1Test.Hello.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace SuperCaisse.Services.Tests.Class1
+namespace SuperCaisse.Services.Tests
 {
     public partial class Class1Test
     {
