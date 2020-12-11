3247ab14e843e3722f588877c19f96c176f54245
Jason Liebault <jason.liebault@simsoft-industry.fr>
2020-12-11 11:19:52 +0100

    Add partial exemple

---
+++ SuperCaisse.Services.Tests/Class1/Class1Test.Hello.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperCaisse.Services.Tests.Class1
+{
+    public partial class Class1Test
+    {
+        [TestMethod]
+        public void Class1_SayHelloWorld()
+        {
+            Assert.AreEqual(_dummy.HelloSentence, "Hello World !");
+        }
+    }
+}
---
+++ SuperCaisse.Services.Tests/Class1/Class1Test.Init.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SuperCaisse.Services.Tests
+{
+    [TestClass]
+    public partial class Class1Test
+    {
+        private Class1 _dummy = new Class1();
+    }
+}
