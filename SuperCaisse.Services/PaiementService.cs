1cb0a7ce72776206957d1d8f2348508288989dc1
Thomas <thomas.cnnt@gmail.com>
2020-12-18 14:09:41 +0100

    WIP PaiementService

---
+++ SuperCaisse.Services/PaiementService.cs
@@ -0,0 +1,14 @@
+﻿using SuperCaisse.Model;
+using System.Linq;
+
+namespace SuperCaisse.Services
+{
+    public class PaiementService
+    {
+        // TODO Extract value as new class
+        public bool PayerPanierByUser(Article[] articles, User user)
+        {
+            var facture = new Facture(articles, user, "")
+        }
+    }
+}
