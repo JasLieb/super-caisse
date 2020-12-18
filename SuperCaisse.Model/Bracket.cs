17d9460dfa6405b5ed95f5316ed2557e7af28809
Nico <skywosh@gmail.com>
2020-12-18 09:35:27 +0100

    Modele Panier

---
+++ SuperCaisse.Model/Bracket.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperCaisse.Model
+{
+    class Bracket
+    {
+        public List<Article> article { get; set; }
+    }
+}
